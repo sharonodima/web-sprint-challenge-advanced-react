10f7f246d3c10398d90aca345f1c8cfed55690f0
Sharon Odima <odimasharon@gmail.com>
2022-05-15 17:19:04 -0400

    Sharon Odima Completed AppFunctional.js

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import axios from "axios"
 export default class AppClass extends React.Component {
   constructor() {
     super();
@@ -7,8 +7,11 @@ export default class AppClass extends React.Component {
       x: 2,
       y: 2,
       error: null,
+      count: 0,
+      email: "",
     }
     this.move = this.move.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   move(direction){
@@ -17,71 +20,103 @@ export default class AppClass extends React.Component {
         if(this.state.x <= 1){
           this.setState({
             ...this.state,
-            error:"You cannot go left"
+            error:"You can't go left"
 
           })
           return;
         }
         this.setState({
           ...this.state,
-          x:this.state.x-1
+          x:this.state.x-1,
+          count: this.state.count + 1,
+          error: null,
         })
         break;
         case "Right":
         if(this.state.x >= 3){
           this.setState({
             ...this.state,
-            error:"You cannot go right"
+            error:"You can't go right"
 
           })
           return;
         }
         this.setState({
           ...this.state,
-          x:this.state.x+1
+          x:this.state.x+1,
+          count: this.state.count + 1,
+          error: null,
         })
         break;
         case "Down":
         if(this.state.y >= 3){
           this.setState({
             ...this.state,
-            error:"You cannot go down"
+            error:"You can't go down"
 
           })
           return;
         }
         this.setState({
           ...this.state,
-          y:this.state.y+1
+          y:this.state.y+1,
+          count: this.state.count + 1,
+          error: null,
         })
         break;
         case "Up":
         if(this.state.y <= 1){
           this.setState({
             ...this.state,
-            error:"You cannot go up"
+            error:"You can't go up"
 
           })
           return;
         }
         this.setState({
           ...this.state,
-          y:this.state.y-1
+          y:this.state.y-1,
+          count: this.state.count + 1,
+          error: null,
         })
         break;
         case "Reset":
-        if(this.state.y = 1){
-          this.setState({
-            ...this.state,
-          })
-          return;
-        }
+        
         this.setState({
           ...this.state,
-          y:this.state.y
+          x: 2,
+          y: 2,
+          count: 0,
+          error: null,
+          email: "",
         })
         break;
     }
+
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    const {x, y, count, email} = this.state
+    axios.post("http://localhost:9000/api/result", {
+      x, y, email,
+      steps: count
+    })
+    .then((response) =>{
+      console.log(response.data)
+      this.setState({
+        ...this.state,
+        error: response.data.message,
+        email: ""
+      })
+    })
+    .catch((err) => {
+      this.setState({
+        ...this.state,
+        error: err.response.data.message,
+        email: ""
+      })
+    })
   }
 
   render() {
@@ -90,25 +125,19 @@ export default class AppClass extends React.Component {
     for(let y = 1; y<=3; y++){
     for(let x = 1; x<=3; x++){
         const isActive = this.state.x === x && this.state.y === y
-        squares.push(<div className="square">{isActive && "B"}</div>)
+        squares.push(<div className={isActive ? "square active" : "square"}>{isActive && "B"}</div>)
       }
     }
     return (
       <div id="wrapper" className={className}>
         <div className="info">
           <h3 id="coordinates">Coordinates ({this.state.x}, {this.state.y})</h3>
-          <h3 id="steps">You moved {this.state.x} times</h3>
+          <h3 id="steps">{this.state.count === 1 ? `You moved ${this.state.count} time` : `You moved ${this.state.count} times`}</h3>
+
+          
+
         </div>
         <div id="grid">
-          {/* <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div> */}
           {squares}
         </div>
         <div className="info">
@@ -121,8 +150,8 @@ export default class AppClass extends React.Component {
           <button id="down" onClick = {() =>this.move("Down")}>DOWN</button>
           <button id="reset" onClick = {() =>this.move("Reset")}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit = {this.handleSubmit}>
+          <input onChange = {(evt) =>this.setState({...this.state, email: evt.target.value})} value = {this.state.email} id="email" type="email" placeholder="type email"></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,37 +1,165 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from "axios"
 
 export default function AppFunctional(props) {
+  const [x, setx] = useState(2);
+  const [y, sety] = useState(2);
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(null);
+  const [count, setCount] = useState(0);
+  const { className } = props;
+  const squares = [];
+  for (let yI = 1; yI <= 3; yI++) {
+    for (let xI = 1; xI <= 3; xI++) {
+      const isActive = x === xI && y === yI;
+      squares.push(<div className={isActive ? "square active" : "square"}>{isActive && "B"}</div>);
+    }
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    //const {x, y, count, email} = this.state
+    axios.post("http://localhost:9000/api/result", {
+      x, y, email,
+      steps: count
+    })
+    .then((response) =>{
+      console.log(response.data)
+      // this.setState({
+      //   ...this.state,
+      //   error: response.data.message
+      // })
+      setError(response.data.message)
+      setEmail("")
+    })
+    .catch((err) => {
+      console.log(err)
+      setError(err.response.data.message)
+      setEmail("")
+    }) 
+  }
+
+  function move(direction){
+    switch (direction) {
+      case "Left":
+        if (x <= 1) {
+          // this.setState({
+          //   ...this.state,
+          //   error: "You cannot go left"
+
+          // });
+          setError("You can't go left")
+          return;
+        }
+        // this.setState({
+        //   ...this.state,
+        //   x: this.state.x - 1,
+        //   count: this.state.count + 1,
+        //   error: null,
+        // });
+        setx(x - 1)
+        setCount(count + 1)
+        setError(null)
+        break;
+      case "Right":
+        if (x >= 3) {
+          // this.setState({
+          //   ...this.state,
+          //   error: "You cannot go right"
+
+          // });
+          setError("You can't go right")
+          return;
+        }
+        // this.setState({
+        //   ...this.state,
+        //   x: this.state.x + 1,
+        //   count: this.state.count + 1,
+        //   error: null,
+        // });
+        setx(x + 1)
+        setCount(count + 1)
+        setError(null)
+        break;
+      case "Down":
+        if (y >= 3) {
+          // this.setState({
+          //   ...this.state,
+          //   error: "You cannot go down"
+
+          // });
+          setError("You can't go down")
+          return;
+        }
+        // this.setState({
+        //   ...this.state,
+        //   y: this.state.y + 1,
+        //   count: this.state.count + 1,
+        //   error: null,
+        // });
+        sety(y + 1)
+        setCount(count + 1)
+        setError(null)
+        break;
+      case "Up":
+        if (y <= 1) {
+          // this.setState({
+          //   ...this.state,
+          //   error: "You cannot go up"
+
+          // });
+          setError("You can't go up")
+          return;
+        }
+        // this.setState({
+        //   ...this.state,
+        //   y: this.state.y - 1,
+        //   count: this.state.count + 1,
+        //   error: null,
+        // });
+        sety(y-1)
+        setCount(count + 1)
+        setError(null)
+        break;
+      case "Reset":
+        // this.setState({
+        //   ...this.state,
+        //   x: 2,
+        //   y: 2,
+        //   count: 0,
+        //   error: null,
+        // });
+        setx(2)
+        sety(2)
+        setCount(0)
+        setError(null)
+        break;
+    }
+
+  }
   return (
-    <div id="wrapper" className={props.className}>
+    <div id="wrapper" className={className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates ({x}, {y})</h3>
+        <h3 id="steps">{count === 1 ? `You moved ${count} time` : `You moved ${count} times`}</h3>
       </div>
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        {squares}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{error}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={() => move("Left")}>LEFT</button>
+        <button id="up" onClick={() => move("Up")}>UP</button>
+        <button id="right" onClick={() => move("Right")}>RIGHT</button>
+        <button id="down" onClick={() => move("Down")}>DOWN</button>
+        <button id="reset" onClick={() => move("Reset")}>reset</button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(evt) => setEmail(evt.target.value)} value={email} id="email" type="email" placeholder="type email"></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
